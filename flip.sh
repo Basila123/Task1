0c354b5d5f17b113c90b29c0ed2989dfdfa8325a
Basila123 <basilanoushad92@gmail.com>
2021-09-18 09:50:01 +0530

    i modified with loops and if

--- flip.sh
+++ flip.sh
@@ -1,6 +1,10 @@
-Flip=$((RANDOM%2))
-if [[ $Flip -eq 0 ]]; then
-    echo "HEADS"
-elif [[ $Flip -eq 1 ]]; then
-    echo "TAILS"
-fi
+
+for((i=1;i<=5;i++))
+do
+	Flip=$((RANDOM%2))
+	if [[ $Flip -eq 0 ]]; then
+    	echo "HEADS"
+	elif [[ $Flip -eq 1 ]]; then
+    	echo "TAILS"
+	fi
+done
